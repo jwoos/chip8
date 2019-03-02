0a09983017becf423096b84f0f869b3fa9decf83
Jun Woo Shin <shin.junwoo97@gmail.com>
2019-03-02 15:04:10 -0500

    hex dump on load

--- main.go
+++ main.go
@@ -9,5 +9,7 @@ import (
 func main() {
 	sys := newSystem()
 	sys.loadROMFile("IBM Logo.ch8")
-	fmt.Println(sys.memory[0x200:])
+	for _, x := range sys.memory[0x200:] {
+		fmt.Printf("%X ", x)
+	}
 }
